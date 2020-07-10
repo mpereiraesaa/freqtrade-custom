3ad138f25bc554a1910af51f315df4a2c17da74a
Manuel Pereira <manuelpereiralds@gmail.com>
2020-07-10 09:44:53 -0300

    Updated build message

--- freqtrade/rpc/rpc_manager.py
+++ freqtrade/rpc/rpc_manager.py
@@ -64,7 +64,7 @@ class RPCManager:
     def startup_messages(self, config: Dict[str, Any], pairlist) -> None:
         self.send_msg({
                 'type': RPCMessageType.WARNING_NOTIFICATION,
-                'status': 'development build: july 6, 2020 08:00'
+                'status': 'development build: july 10, 2020 10:00'
             })
         if config['dry_run']:
             self.send_msg({
